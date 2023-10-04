9141809dd1a9e3e57909ac617ddc84765e7b9e63
Nikola Kolev <nickkolev06@abv.bg>
2023-10-04 11:55:46 +0300

    finished with 07-Unit Testing And Error Handling exercise

--- JS-Advanced/07.UnitTestingAndErrorHandling/Ex/06.Dynamic-Validation/app.js
+++ JS-Advanced/07.UnitTestingAndErrorHandling/Ex/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+    
+    let inputEmailElement = document.getElementById('email');
+
+    inputEmailElement.addEventListener('change', onChange);
+
+    function onChange(e) {
+        const targetStringValue = e.target.value;
+
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        const isRemoveOperation = pattern.test(targetStringValue);
+
+        const operation = isRemoveOperation ? "remove" : "add";
+        e.target.classList[operation]("error");
+    }
 }
