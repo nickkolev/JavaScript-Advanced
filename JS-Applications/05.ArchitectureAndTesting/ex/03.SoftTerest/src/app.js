955120721804370314fbaef3d7cd123c34767836
Nikola Kolev <nickkolev06@abv.bg>
2023-11-09 22:08:36 +0200

    finished with SoftTerest

---
+++ JS-Applications/05.ArchitectureAndTesting/ex/03.SoftTerest/src/app.js
@@ -0,0 +1,30 @@
+import { logout } from "./api/users.js";
+import { intitialize } from "./router.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
+
+document.getElementById("views").remove();
+
+const links = {
+    "/": showHome,
+    "/catalog": showCatalog,
+    "/login": showLogin,
+    "/register": showRegister,
+    "/details": showDetails,
+    "/create": showCreate,
+    "/logout": onLogOut,
+};
+
+const router = intitialize(links);
+router.updateNav();
+router.goTo("/");
+
+function onLogOut() {
+    logout();
+    router.updateNav();
+    router.goTo("/");
+}
