c97ebea4804b35afbb94d99b7a012dc34139259a
Nikola Kolev <nickkolev06@abv.bg>
2023-11-12 16:24:31 +0200

    4/6

--- JS-Applications/06.CliendSideRendering/ex/03.Search-in-List/search.js
+++ JS-Applications/06.CliendSideRendering/ex/03.Search-in-List/search.js
@@ -1,3 +1,41 @@
+import { html, render } from "../../../../node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js";
+
+const searchTemplate = (towns, match) => html`
+<article>
+        <div id="towns">
+            <ul>
+                ${towns.map(t => itemTemplate(t, match))}
+            </ul>
+        </div>
+        <input type="text" id="searchText" />
+        <button @click = ${search}>Search</button>
+        <div id="result">${countMatches(towns, match)}</div>
+    </article>
+`;
+
+const itemTemplate = (name, match) => html`
+   <li class = ${(match && name.toLowerCase().includes(match.toLowerCase())) ? 'active' : ''}>${name}</li>
+`
+
+const main = document.body;
+update();
+
+// Работата на тази ф-я е да стартира темплейта (searchTemplate) и да рендерира полученият от темплейта резултат в бодито на документа
+function update(match = '') {
+   const result = searchTemplate(towns, match);
+   render(result, main);
+}
+
+// Взимаме това, което се търси (въведеното в Search полето) и го подаваме на update функцията
 function search() {
-   // TODO
+
+   const match = document.getElementById('searchText').value;
+   update(match);
+}
+
+// Ф-я, която брои колко мача имаме.
+function countMatches(towns, match) {
+   const matches = towns.filter(t => match && t.toLowerCase().includes(match.toLowerCase())).length;
+   return matches ? `${matches} matches found` : '';
 }
--- JS-Applications/06.CliendSideRendering/ex/04.Fill-Dropdown/dropdown.js
+++ JS-Applications/06.CliendSideRendering/ex/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,43 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "../../../../node_modules/lit-html/lit-html.js";
+
+const URL = "http://localhost:3030/jsonstore/advanced/dropdown";
+
+async function getOptions() {
+    const response = await fetch(URL);
+    return await response.json();
+}
+
+const selectTemplate = (data) => html`
+    <select id="menu">
+        ${data.map((el) => html`<option value=${el._id}>${el.text}</option>`)}
+    </select>
+`;
+
+const options = Object.values(await getOptions());
+const main = document.querySelector("div");
+
+update(options);
+
+function update(options) {
+    const result = selectTemplate(options);
+    render(result, main);
+}
+
+// Add new element
+document.querySelector("form").addEventListener("submit", addItem);
+
+async function addItem(event) {
+    event.preventDefault();
+
+    let text = document.getElementById("itemText").value;
+    if (text !== "") {
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ text }),
+        });
+        options.push(await response.json());
+        update(options);
+    }
+    document.getElementById("itemText").value = "";
 }
