f3634b0f51b2a4f8ec84a96f59930f1936dd4eff
Nikola Kolev <nickkolev06@abv.bg>
2023-11-20 23:02:18 +0200

    first exam - work in progress

---
+++ JS-Applications/99.ExamPrep/2021-12-04_MusicApp/src/services/albumService.js
@@ -0,0 +1,11 @@
+import * as request from "./requester.js";
+
+const baseUrl = "http://localhost:3030/data/albums";
+
+export const getAll = () => {
+    return request.get(`${baseUrl}?sortBy=_createdOn%20desc&distinct=name`);
+}
+
+export const getOne = (albumId) => request.get(`${baseUrl}/${albumId}`);
+
+export const create = (albumData) => request.post(baseUrl, albumData);
---
+++ JS-Applications/99.ExamPrep/2021-12-04_MusicApp/src/views/editView.js
@@ -0,0 +1,52 @@
+import { html, nothing } from "../../node_modules/lit-html/lit-html.js";
+import * as albumService from "../services/albumService.js";
+import { albumIsInvalid } from "../utils/validators.js";
+
+const editTemplate = (album, submitHandler) => html`
+    <section class="editPage">
+        <form @submit=${submitHandler}, method='POST'>
+            <fieldset>
+                <legend>Edit Album</legend>
+
+                <div class="container">
+                    <label for="name" class="vhide">Album name</label>
+                    <input id="name" name="name" value=${album.name} class="name" type="text" value="In These Silent Days">
+
+                    <label for="imgUrl" class="vhide">Image Url</label>
+                    <input id="imgUrl" name="imgUrl" value=${album.imgUrl} class="imgUrl" type="text" value="./img/BrandiCarlile.png">
+
+                    <label for="price" class="vhide">Price</label>
+                    <input id="price" name="price" value=${album.price} class="price" type="text" value="12.80">
+
+                    <label for="releaseDate" class="vhide">Release date</label>
+                    <input id="releaseDate" name="releaseDate" value=${album.releaseDate} class="releaseDate" type="text" value="October 1, 2021">
+
+                    <label for="artist" class="vhide">Artist</label>
+                    <input id="artist" name="artist" value=${album.artist} class="artist" type="text" value="Brandi Carlile">
+
+                    <label for="genre" class="vhide">Genre</label>
+                    <input id="genre" name="genre" value=${album.genre} class="genre" type="text" value="Low Country Sound Music">
+
+                    <label for="description" class="vhide">Description</label>
+                    <textarea name="description" .value=${album.description} class="description" rows="10" cols="10"></textarea>
+
+                    <button class="edit-album" type="submit">Edit Album</button>
+                </div>
+            </fieldset>
+        </form>
+    </section>
+`;
+
+
+export const editView = (ctx) => {
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const albumData = Object.fromEntries(new FormData(e.currentTarget));
+
+    }
+
+    albumService.getOne(ctx.params.albumId).then((album) => {
+        ctx.render(editTemplate(album, ctx.user));
+    });
+}
