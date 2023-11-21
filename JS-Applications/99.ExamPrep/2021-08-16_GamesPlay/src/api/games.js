cde8227a8a046f4ec4a2c0fd0df557a707c25a29
Nikola Kolev <nickkolev06@abv.bg>
2023-11-22 00:01:29 +0200

    work in progress

---
+++ JS-Applications/99.ExamPrep/2021-08-16_GamesPlay/src/api/games.js
@@ -0,0 +1,24 @@
+import * as api from './api.js';
+
+const endpoints = {
+    recent: '/data/games?sortBy=_createdOn%20desc&distinct=category',
+    games: '/data/games?sortBy=_createdOn%20desc',
+    create: '/data/games',
+    byId: '/data/games/'
+};
+
+export async function getRecent() {
+    return api.get(endpoints.recent);
+};
+
+export async function getAll() {
+    return api.get(endpoints.games);
+}
+
+export async function getById(id) {
+    return api.get(endpoints.byId + id);
+}
+
+export async function create(data) {
+    return api.post(endpoints.create, data)
+}
