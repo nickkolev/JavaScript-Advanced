912acf9916924d4884739690cc4b784e779bb9bf
Nikola Kolev <nickkolev06@abv.bg>
2023-12-05 19:38:36 +0200

    a

--- JS-Applications/99.ExamPrep/2022-12-03_MusicLibrary/src/views/create.js
+++ JS-Applications/99.ExamPrep/2022-12-03_MusicLibrary/src/views/create.js
@@ -5,49 +5,19 @@ import * as albumsService from "../api/albums.js";
 const createTemplate = (onSubmit) => html`
     <section id="create">
         <div class="form">
-            <h2>Add Album</h2>
-            <form @submit=${onSubmit} class="create-form">
-                <input
-                    type="text"
-                    name="singer"
-                    id="album-singer"
-                    placeholder="Singer/Band"
-                />
-                <input
-                    type="text"
-                    name="album"
-                    id="album-album"
-                    placeholder="Album"
-                />
-                <input
-                    type="text"
-                    name="imageUrl"
-                    id="album-img"
-                    placeholder="Image url"
-                />
-                <input
-                    type="text"
-                    name="release"
-                    id="album-release"
-                    placeholder="Release date"
-                />
-                <input
-                    type="text"
-                    name="label"
-                    id="album-label"
-                    placeholder="Label"
-                />
-                <input
-                    type="text"
-                    name="sales"
-                    id="album-sales"
-                    placeholder="Sales"
-                />
+          <h2>Add Album</h2>
+          <form @submit=${onSubmit} class="create-form">
+            <input type="text" name="singer" id="album-singer" placeholder="Singer/Band" />
+            <input type="text" name="album" id="album-album" placeholder="Album" />
+            <input type="text" name="imageUrl" id="album-img" placeholder="Image url" />
+            <input type="text" name="release" id="album-release" placeholder="Release date" />
+            <input type="text" name="label" id="album-label" placeholder="Label" />
+            <input type="text" name="sales" id="album-sales" placeholder="Sales" />
 
-                <button type="submit">post</button>
-            </form>
+            <button type="submit">post</button>
+          </form>
         </div>
-    </section>
+      </section>
 `;
 
 export function createPage(ctx) {
